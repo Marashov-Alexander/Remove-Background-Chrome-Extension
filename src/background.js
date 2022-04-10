c19103bb885e631afa42242f3f4ad938e4fcc0f2
alexander.marashov <alexander.marashov@corp.mail.ru>
2022-04-10 12:11:31 +0300

    All images page

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
-const BASE_ENDPOINT = 'https://b9a4-95-55-86-98.ngrok.io';
-let username;
+const BASE_ENDPOINT = 'https://b7e5-31-134-188-214.ngrok.io';
+let authorization = null
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -9,21 +9,18 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-chrome.identity.getProfileUserInfo((userInfo) => {
-  username = 
-    userInfo.email 
-      || ('unauthorized' + String(Math.random()).split('.')[1]);
-});
-
 chrome.contextMenus.onClicked.addListener(async (imageInfo) => {
+  if (authorization === null) {
+    await register()
+  }
   return fetch(BASE_ENDPOINT + '/process-image', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': authorization
     },
     body: JSON.stringify({
-      username: username,
       imageUrl: imageInfo.srcUrl
     })
   })
@@ -31,12 +28,42 @@ chrome.contextMenus.onClicked.addListener(async (imageInfo) => {
     return rawResp.json()
   })
   .then(respBody => {
+    console.log(respBody);
     if (respBody?.imageUrl) {
       chrome.tabs.create({ url: BASE_ENDPOINT + '/get-image' 
-        + '?imageName=' + respBody.imageUrl });
-    } else {
-      console.log(respBody?.message);
+        + '?imageName=' + respBody.imageUrl + '&authorization=' + authorization });
     }
   })
-  .catch(err => {});
+  .catch(err => {console.log(err)});
 });
+
+async function register() {
+  const username = CreateUUID()
+  const password = CreateUUID()
+  return fetch(BASE_ENDPOINT + '/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+      .then(rawResp => {
+          console.log(rawResp)
+          authorization = btoa(username + ':' + password)
+      })
+      .catch(err => { console.log(err) });
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({ url: BASE_ENDPOINT + '/get-all-images' + '?authorization=' + authorization})
+});
+
+function CreateUUID() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
